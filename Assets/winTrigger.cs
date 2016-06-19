bff28542810878ac84bf74c098a31fc95d0c3436
jankaVar <janavareckova@yahoo.com>
2016-06-19 10:35:00 +0200

    more object with Tag player have to be at the win cube to continue to another level

--- Assets/winTrigger.cs
+++ Assets/winTrigger.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class winTrigger : MonoBehaviour {
 
+    List<Collider2D> particlesIn;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("You won!!!");
+        //Debug.Log("You won!!!");
 
     }
 
@@ -17,36 +20,47 @@ public class winTrigger : MonoBehaviour {
         BoxCollider2D win = wc.GetComponent<BoxCollider2D>();
 
         float rightWin = win.bounds.center.x - win.bounds.extents.x;
-        float rightOther = other.bounds.center.x - other.bounds.extents.x;
         float leftWin = win.bounds.center.x + win.bounds.extents.x;
-        float leftOther = other.bounds.center.x + other.bounds.extents.x;
-
-        float topOther = other.bounds.center.y + other.bounds.extents.y;
         float topWin = win.bounds.center.y + win.bounds.extents.y;
-        float bottompOther = other.bounds.center.y - other.bounds.extents.y;
         float bottomWin = win.bounds.center.y - win.bounds.extents.y;
-        
-        if ((rightWin < rightOther) &&
-            (topWin > topOther) &&
-            (leftWin > leftOther) &&
-            (bottomWin < bottompOther)) {
-
-            Debug.Log("You are in!!");
 
 
+            float rightOther = other.bounds.center.x - other.bounds.extents.x;
+            float leftOther = other.bounds.center.x + other.bounds.extents.x;
+            float topOther = other.bounds.center.y + other.bounds.extents.y;
+            float bottompOther = other.bounds.center.y - other.bounds.extents.y;
+
+            if ((rightWin < rightOther) &&
+                (topWin > topOther) &&
+                (leftWin > leftOther) &&
+                (bottomWin < bottompOther))
+        {
+            Debug.Log("You are in!! ");
+
+            if (particlesIn.IndexOf(other) == -1)
+                particlesIn.Add(other);
+        }
+
+        GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+
+
+        if (particlesIn.Count == player.Length) {
             SceneManager.LoadScene("Level2");
-
-        } 
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
+        if (particlesIn.IndexOf(other) > -1)
+            particlesIn.Remove(other);
+
     }
 
     // Use this for initialization
     void Start () {
-	
-	}
+        particlesIn = new List<Collider2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
